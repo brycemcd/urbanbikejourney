80d8f374b0a468b9687b6a4e85b94380a612e17d
Bryce McDonnell <bryce@bridgetownint.com>
2014-07-20 17:14:15 -0400

    VERY much a Work in Progress, directions

--- public/angular_apps/controllers/directions.js
+++ public/angular_apps/controllers/directions.js
@@ -1,6 +1,5 @@
 function DirectionsCtrl($scope, $http) {
   $scope.directionsModel = {
-    {
       origin: "Chicago, IL",
       destination: "Los Angeles, CA",
       waypoints: [
@@ -12,19 +11,46 @@ function DirectionsCtrl($scope, $http) {
         stopover:true
       }],
       provideRouteAlternatives: false,
-      travelMode: TravelMode.BICYCLING,
-      unitSystem: UnitSystem.IMPERIAL
-    }
+      travelMode: google.maps.TravelMode.BICYCLING,
+      unitSystem: google.maps.UnitSystem.IMPERIAL
   };
+
+  $scope.brooklyn = new google.maps.LatLng(40.6928, -73.9903);
+  $scope.nyc = new google.maps.LatLng(40.7127, -74.0059);
+  $scope.directionsDisplay = new google.maps.DirectionsRenderer();
+  $scope.directionsService = new google.maps.DirectionsService();
+
+
   $scope.getDirections = function() {
     $scope._showStatement();
 
     var map;
+    
     var mapOptions = {
           zoom: 10,
-          center: new google.maps.LatLng(40.7127, -74.0059)
+          center: $scope.nyc
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    $scope.directionsDisplay.setMap(map);
+
+    $scope.calcRoute();
+  };
+
+  $scope.calcRoute = function() {
+    var request = {
+      origin: $scope.brooklyn,
+      destination: $scope.nyc,
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+
+    $scope.directionsService.route(request, function(result, status) {
+      console.log("request: ", request);
+      console.log("result: ", result);
+      console.log("status: ", status);
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(result);
+      }
+    });
   };
 
   $scope._showStatement = function() {
