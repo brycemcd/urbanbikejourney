ea8bfec120336e775b01afd2cdaeb4e51e0e35d7
Bryce McDonnell <bryce@bridgetownint.com>
2014-07-26 13:22:31 -0400

    Adds an elevation chart! (for fun)

--- public/angular_apps/controllers/directions.js
+++ public/angular_apps/controllers/directions.js
@@ -1,3 +1,5 @@
+google.load('visualization', '1', {packages: ['columnchart']});
+
 angular.module('urbanBikeJourney.controllers', []).
   controller('DirectionsCtrl', ['$scope', function($scope, $http) {
 
@@ -14,6 +16,7 @@ angular.module('urbanBikeJourney.controllers', []).
   $scope.nyc = new google.maps.LatLng(40.7127, -74.0059);
   $scope.directionsDisplay = new google.maps.DirectionsRenderer();
   $scope.directionsService = new google.maps.DirectionsService();
+  $scope.elevator = new google.maps.ElevationService();
 
 
   $scope.getDirections = function() {
@@ -31,6 +34,54 @@ angular.module('urbanBikeJourney.controllers', []).
     $scope.calcRoute();
   };
 
+  $scope.plotElevation = function(results, status) {
+    // Takes an array of ElevationResult objects, draws the path on the map
+    // and plots the elevation profile on a Visualization API ColumnChart.
+    console.log('elevation result: ', results);
+    console.log('elevation status: ', status);
+    if (status != google.maps.ElevationStatus.OK) {
+      return;
+    }
+    var elevations = results;
+
+    // Extract the elevation samples from the returned results
+    // and store them in an array of LatLngs.
+    var elevationPath = [];
+    for (var i = 0; i < results.length; i++) {
+      elevationPath.push(elevations[i].location);
+    }
+
+    // Display a polyline of the elevation path.
+    //var pathOptions = {
+      //path: elevationPath,
+      //strokeColor: '#0000CC',
+      //opacity: 0.4,
+      //map: map
+    //}
+    //polyline = new google.maps.Polyline(pathOptions);
+
+    // Extract the data from which to populate the chart.
+    // Because the samples are equidistant, the 'Sample'
+    // column here does double duty as distance along the
+    // X axis.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Sample');
+    data.addColumn('number', 'Elevation');
+    for (var i = 0; i < results.length; i++) {
+      data.addRow(['', elevations[i].elevation]);
+    }
+
+    chart = new google.visualization.ColumnChart(document.getElementById('elevation-graph'));
+    // Draw the chart using the data within its DIV.
+    document.getElementById('elevation-graph').style.display = 'block';
+    chart.draw(data, {
+      height: 150,
+      legend: 'none',
+      titleY: 'Elevation (m)'
+    });
+
+  };
+
   $scope.calcRoute = function() {
     var request = {
       origin: $scope.directionsModel.origin,
@@ -38,9 +89,19 @@ angular.module('urbanBikeJourney.controllers', []).
       travelMode: google.maps.TravelMode.BICYCLING
     };
 
+
     $scope.directionsService.route(request, function(result, status) {
       console.log("result: ", result.routes[0]);
       $scope.directionsModel.route = result.routes[0];
+
+      var path = result.routes[0].legs[0].steps[0].lat_lngs
+      var pathRequest = {
+        'path' : path,
+        'samples' : 256
+      }
+
+      $scope.elevator.getElevationAlongPath(pathRequest, $scope.plotElevation);
+
       $scope.$apply(); // doesn't seem like this should be needed
       if (status == google.maps.DirectionsStatus.OK) {
         $scope.directionsDisplay.setDirections(result);
@@ -51,4 +112,5 @@ angular.module('urbanBikeJourney.controllers', []).
   $scope._showStatement = function() {
     $('#directions-statement').show();
   };
+
 }]);
