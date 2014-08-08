bfe3f1384bdf56fbe565b79d9e70b031043e877a
Chad W Pry <chad.pry@gmail.com>
2014-08-07 20:18:52 -0400

    update sinatra app with root and view folder

--- lib/urban_bike_journey/application.rb
+++ lib/urban_bike_journey/application.rb
@@ -2,6 +2,14 @@ require 'sinatra/base'
 
 module UrbanBikeJourney
   class Application < Sinatra::Base
+
+    set :root,  "#{File.dirname(__FILE__)}/../../"
+    set :views, Proc.new { File.join(root, "views") }
+
+    configure do
+      enable :logging, :show_exceptions
+    end
+
     get '/' do
       erb :index
     end
